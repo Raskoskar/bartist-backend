624191aca79155604a71c26dfa342cf612963a4f
Emilie <emilie.pechot@gmail.com>
2024-04-18 17:44:37 +0200

    routes display et update bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,7 +5,10 @@ const Venue = require("../models/VenueModel");
 const Artist = require("../models/ArtistModel");
 const Booking = require("../models/BookingModel");
 
+// Pour afficher tous les bookings du user dans la page "Mes propositions"
 exports.displayAllBookings = async (req, res) => {
+    console.log(req.body.isVenue)
+    console.log(req.body.token)
     if(req.body.isVenue) { // si le user connecté est un établissement
         Venue.findOne({token: req.body.token})
             .then(dataVenue => {
@@ -40,4 +43,17 @@ exports.displayAllBookings = async (req, res) => {
         })
     }
 
+}
+
+// Pour accepter ou refuser un booking en changeant son statut
+exports.updateBookingStatus = async (req, res) => {
+    Booking.findById(req.body._id)
+        .then(dataBooking => {
+            if(dataBooking) {
+                dataBooking.status = req.body.status;
+                res.json({ result: true, dataBooking })
+            } else {
+                res.json({ result: false, error: 'Booking not found' })
+            }
+        })
 }
